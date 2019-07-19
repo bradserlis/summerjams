3fe31665e1a266b78eca2e81f7d65db93780de0b
bradserlis <bradserlis@yahoo.com>
2019-07-18 19:37:24 -0700

    setup basic api call and response

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config(); //loads the .env file
+
+const express = require('express'),
+  app = express(),
+  port = 3000,
+
+  request = require('request');
+
+
 
 app.get('/', (req, res) => res.send('You are in Bradley world!'))
 app.get('/summerjams', (req, res) => res.send('Welcome to SUMMER JAMS!'))
+app.get('/:id', (req, res) => 
+  request(`https://tastedive.com/api/similar?k=${process.env.MY_KEY}&q=${req.params.id}`, (error, response, body) => {
+    let bodyObj = JSON.parse(body);
+    res.send(bodyObj)
+  })
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
