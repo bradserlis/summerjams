0417f7cc5b219d2666c147334d3730e32946358d
bradserlis <bradserlis@yahoo.com>
2019-07-29 18:25:58 -0700

    update for class to play with

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.get('/summerjams', (req, res) => res.send('Welcome to SUMMER JAMS!'))
 app.get('/:id', (req, res) => 
   request(`https://tastedive.com/api/similar?k=${process.env.MY_KEY}&q=${req.params.id}`, (error, response, body) => {
     let bodyObj = JSON.parse(body);
-    res.send(bodyObj)
+    console.log('what is the bodyObj.similar.results?', bodyObj.Similar.Results);
+    bodyObj.Similar.Results.length === 0 ? res.send("We couldn't find a Summer Jam match for that one...try again?") : res.send(bodyObj);
   })
 )
 
